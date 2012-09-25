a53a5929d7fe5acc11524f453e961359ba6f9470
gcmalloc <gcmalloc@gmail.com>
2012-09-25 14:18:18 +0200

    updating version and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
-setup(name='epfl-menu',
-      version = '0.0.1',
+setup(
+      name='epfl-moodle',
+      version = '0.1.0',
       description = 'A simple interface to moodle',
       author = 'gcmalloc',
       url = 'http://github.com/gcmalloc/epfl-moodle',
