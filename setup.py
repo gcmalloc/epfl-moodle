c3eb8310d85f60d881c6b9b9db4a01dc2e7a1d49
gcmalloc <gcmalloc@gmail.com>
2013-04-11 00:37:58 +0200

    renaming module to stop conflicting with menu

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(
       name='epfl-moodle',
-      version = '0.4.0',
+      version = '0.4.2',
       description = 'A simple interface to moodle',
       author = 'gcmalloc',
       url = 'http://github.com/gcmalloc/epfl-moodle',
-      py_modules = ['epfl.moodle'],
+      py_modules = ['epfl-moodle.moodle'],
       install_requires=['BeautifulSoup4', 'requests'],
       scripts = ['bin/moodle'])
