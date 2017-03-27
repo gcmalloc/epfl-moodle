a07c56ebe7a2861732b89d0ec8b21000040ee1d2
Antoine Albertelli <antoine@antoinealb.net>
2017-03-27 14:41:09 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
       name='epfl-moodle',
-      version = '0.4.4',
+      version = '0.5.0',
       description = 'A simple interface to moodle',
       author = 'gcmalloc',
       url = 'http://github.com/gcmalloc/epfl-moodle',
